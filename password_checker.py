11ffa47113213873c9844b750c0379167d8739be
theobarreto <87341387+theobarreto@users.noreply.github.com>
2023-12-03 14:53:53 -0300

    Update password_checker.py

--- password_checker.py
+++ password_checker.py
@@ -1,26 +1,31 @@
 import hashlib
 import getpass
+import sys
 
 def encrypt_content(content):
     hashed_content = hashlib.sha256(content.encode()).hexdigest()
     return hashed_content
 
-def read_password_from_file(file_path):
-    # Lê o conteúdo criptografado do arquivo
+def read_content_from_file(file_path):
+    # Lê o conteúdo do arquivo
     with open(file_path, 'r') as file:
         return file.read().strip()
 
 def save_content_to_file(content, file_path):
-    # Salva o conteúdo criptografado em um arquivo
+    # Salva o conteúdo em um arquivo
     with open(file_path, 'w') as file:
         file.write(content)
 
 def main():
-    encrypted_file_path = 'encrypted_file.txt'
+    # Verifica se foram fornecidos argumentos de linha de comando
+    if len(sys.argv) < 3:
+        print("Uso: python script.py <caminho_do_arquivo_criptografado> <caminho_do_arquivo_de_texto>")
+        sys.exit(1)
 
-    # Branch 1: Solicita ao usuário para fornecer um arquivo de texto e criptografa o conteúdo
-    user_file_path = input("Digite o caminho do arquivo de texto: ")
+    encrypted_file_path = sys.argv[1]
+    user_file_path = sys.argv[2]
 
+    # Solicita ao usuário para fornecer uma senha e criptografa o conteúdo
     with open(user_file_path, 'r') as user_file:
         content_to_encrypt = user_file.read()
 
@@ -29,10 +34,10 @@ def main():
     save_content_to_file(encrypted_content, encrypted_file_path)
     print("Conteúdo criptografado e armazenado com sucesso.")
 
-    # Branch 2: Realiza o teste de comparação internamente
-    stored_content = read_password_from_file(encrypted_file_path)
+    # Realiza o teste de comparação internamente
+    stored_content = read_content_from_file(encrypted_file_path)
 
-    # Simule o mesmo processo de criptografia para a senha fornecida
+    # Solicita ao usuário para fornecer uma senha para teste
     user_password = getpass.getpass("Digite a senha para teste: ")
     encrypted_user_password = encrypt_content(user_password)
 
