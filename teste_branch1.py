bc31761ef099c83b76731e9062d3ad3579021dfe
theobarreto <87341387+theobarreto@users.noreply.github.com>
2023-12-03 14:55:40 -0300

    Update teste_branch1.py

--- teste_branch1.py
+++ teste_branch1.py
@@ -3,10 +3,13 @@ from unittest.mock import patch
 from password_checker import main
 
 class TestBranch1(unittest.TestCase):
-    @patch('builtins.input', side_effect=['path/to/your/testfile.txt'])
-    def test_branch_1(self, mock_input):
-        with patch('getpass.getpass', return_value='password123'):
-            self.assertEqual(main(), 0)
+    def test_branch_1(self):
+        # Substitua 'caminho/para/sua/testfile.txt' pelo caminho real fornecido no github actions
+        user_file_path = 'path/to/your/testfile.txt'
+
+        with patch('builtins.input', return_value=user_file_path):
+            with patch('getpass.getpass', return_value='password123'):
+                self.assertEqual(main(), 0)
 
 if __name__ == '__main__':
     unittest.main()
